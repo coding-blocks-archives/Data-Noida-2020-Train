7c09ed713797b23a7fa1f6c92a151c64b241c125
KeenWarrior <anujgargcse@gmail.com>
2020-02-25 13:12:48 +0530

    l 21

--- lecture_21/BST.py
+++ lecture_21/BST.py
@@ -25,18 +25,55 @@ class BST:
             if node.value < value:
                 node.right = self.__add(value, node.right)
 
-            node.height = max(BST.height(node.left), BST.height(node.right)) + 1
+            node.height = max(self.height(node.left), self.height(node.right)) + 1
 
-        return node
+        return self.balance(node)
 
-    @staticmethod
-    def height(node):
-
+    def height(self, node):
         if node == None:
             return 0
         else:
             return node.height
 
+    def right_rotate(self, node):
+        x = node
+        y = x.left
+        t2 = y.right
+
+        y.right = x
+        x.left = t2
+
+        x.height = max(self.height(x.left), self.height(x.right)) + 1
+        y.height = max(self.height(y.left), self.height(y.right)) + 1
+
+        return y
+
+    def left_rotate(self, node):
+        y = node
+        x = y.right
+        t2 = x.left
+
+        x.left = y
+        y.right = t2
+
+        y.height = max(self.height(y.left), self.height(y.right)) + 1
+        x.height = max(self.height(x.left), self.height(x.right)) + 1
+
+        return x
+
+    def balance(self, node):
+
+        if (self.height(node.left) - self.height(node.right)) > 1:
+            if (self.height(node.left.left) - self.height(node.left.right)) < 0:
+                node.left = self.left_rotate(node.left)
+            return self.right_rotate(node)
+
+        elif (self.height(node.left) - self.height(node.right)) < -1:
+            if (self.height(node.right.left) - self.height(node.right.right)) > 0:
+                node.right = self.right_rotate(node.right)
+            return self.left_rotate(node)
+
+        return node
 
     def display(self):
         self.__display(self.root)
@@ -47,8 +84,25 @@ class BST:
         if node == None:
             return
 
-        print(indent, node.value, position, node.height)
+        print(indent, node.value, position, node.height, self.height(node.left)- self.height(node.right))
         self.__display(node.left, indent+"\t", "left")
         self.__display(node.right, indent+"\t", "right")
+
+    def balanced(self):
+        return self.__balanced(self.root)
+
+    def __balanced(self, node):
+        if node == None:
+            return True
+
+        current = abs(self.height(node.left) - self.height(node.right)) < 2
+        left = self.__balanced(node.left)
+        right = self.__balanced(node.right)
+
+        return current and left and right
+
+
+
+
 
 
